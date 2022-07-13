09e45835275cf9a1f6609f1343c0d11a045851e3
PJunyeong <j_aion@naver.com>
2022-07-14 08:39:22 +0900

    #5 feat : 텍스트 필드 borderline 패딩으로 간격 맞추기

--- FruitFruit/FruitFruit/Models/FruitTextField.swift
+++ FruitFruit/FruitFruit/Models/FruitTextField.swift
@@ -22,9 +22,9 @@ class FruitTextField: UITextField {
         
         addSubview(bottomBorder)
         
-        bottomBorder.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        bottomBorder.topAnchor.constraint(equalTo: bottomAnchor, constant: 8).isActive = true
         bottomBorder.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
         bottomBorder.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
-        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true // Set Border-Strength
+        bottomBorder.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
 }
