e87dea21481e46f7266e3276d4ef457414ab05e0
PJunyeong <j_aion@naver.com>
2022-07-13 15:15:43 +0900

    #3 파이어베이스 configure

--- FruitFruit/FruitFruit/AppDelegate.swift
+++ FruitFruit/FruitFruit/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
         return true
     }
 
