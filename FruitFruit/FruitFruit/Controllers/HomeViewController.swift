ffae1cfe9193e679b751296bc12570e219292aef
PJunyeong <j_aion@naver.com>
2022-07-15 22:23:23 +0900

    #11 feat : 홈뷰 주문 상태 라벨 - 버튼 그림자 및 테두리

--- FruitFruit/FruitFruit/Controllers/HomeViewController.swift
+++ FruitFruit/FruitFruit/Controllers/HomeViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 class HomeViewController: UIViewController {
     // MARK: - PROPERTIES
     let fruitStautsLabel: FruitStatusLabel = {
-        let statusLabel = FruitStatusLabel(frame: CGRect(x: 0, y: 0, width: 342, height: 68))
+        let statusLabel = FruitStatusLabel(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 48, height: 68))
+        // TODO: UIScreen을 사용하지 않고 LifeCycle에서 view.bounds를 사용해서 Init하기
         statusLabel.translatesAutoresizingMaskIntoConstraints = false
         return statusLabel
     }()
@@ -34,6 +35,7 @@ class HomeViewController: UIViewController {
     }
     
     private func setFruitStatusLabel() {
+        fruitStautsLabel.frame = CGRect(x: 0, y: 0, width: view.bounds.width - 48, height: 68)
         view.addSubview(fruitStautsLabel)
         fruitStautsLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - 48).isActive = true
         fruitStautsLabel.heightAnchor.constraint(equalToConstant: 68).isActive = true
