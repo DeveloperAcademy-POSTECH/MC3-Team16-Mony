ae5c92b41f6fa7ab85043fa19b57aef7c5f9f7d4
PJunyeong <j_aion@naver.com>
2022-07-15 21:00:18 +0900

    #11 홈뷰 타이틀 라벨 UI

--- FruitFruit/FruitFruit/Controllers/HomeViewController.swift
+++ FruitFruit/FruitFruit/Controllers/HomeViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var homeTitleLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.applyBackgroundGradient()
+        setHomeViewUI()
+    }
+    
+    private func setHomeViewUI() {
+        homeTitleLabel.font = UIFont.preferredFont(for: .title1, weight: .bold)
     }
 }
