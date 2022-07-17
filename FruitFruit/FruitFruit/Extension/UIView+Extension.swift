637c5ca75ad0d9181ffe3f75bfcc28a448e42705
PJunyeong <j_aion@naver.com>
2022-07-17 22:57:24 +0900

    #5 style : 버튼 활성화 그레디언트 디테일 추가

--- FruitFruit/FruitFruit/Extension/UIView+Extension.swift
+++ FruitFruit/FruitFruit/Extension/UIView+Extension.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIView {
     
     func applyButtonGradient(colors: [UIColor]) -> CAGradientLayer {
-        return self.applyButtonGradient(colors: colors, locations: nil)
+        return self.applyButtonGradient(colors: colors, locations: [0, 1])
     }
     
     func applyButtonGradient(colors: [UIColor], locations: [NSNumber]?) -> CAGradientLayer {
@@ -19,11 +19,12 @@ extension UIView {
         graident.colors = colors.map {$0.cgColor}
         graident.locations = locations
         graident.cornerRadius = 16
+        graident.startPoint = CGPoint(x: 0.25, y: 0.5)
+        graident.endPoint = CGPoint(x: 0.75, y: 0.5)
         return graident
     }
     // How to use
     // 1. let gradient = button.applyButtonGraident(colors: [yourColor])
     // 2. button.layer.insertSublayer(graident, at: 0)
     // 버튼 레이어의 가장 상단부에 위치 백그라운드 컬러를 그레디언트로 주기
-
 }
---
+++ FruitFruit/FruitFruit/Views/FruitSelectButton.swift
@@ -0,0 +1,8 @@
+//
+//  FruitSelectButton.swift
+//  FruitFruit
+//
+//  Created by Junyeong Park on 2022/07/17.
+//
+
+import Foundation
