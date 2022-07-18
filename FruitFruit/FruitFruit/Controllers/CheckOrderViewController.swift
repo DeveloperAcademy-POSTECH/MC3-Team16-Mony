410feb1bce27b456413ff9c7c2a2c2fd10c829e1
Guel-git <eccyuna@gmail.com>
2022-07-18 16:03:19 +0900

    [feat] #13 background color gradient 추가 - 내부에서만 사용되는 function에 private 추가 - 추후에 extension 추가 예정ㅑ

--- FruitFruit/FruitFruit/Controllers/CheckOrderViewController.swift
+++ FruitFruit/FruitFruit/Controllers/CheckOrderViewController.swift
@@ -14,36 +14,42 @@ class CheckOrderViewController: UIViewController {
     @IBOutlet weak var checkOrderSecondaryLabel: UILabel!
     @IBOutlet weak var checkOrderButton: UIButton!
     
+    
+    @IBAction func onCheckOrderButtonClicked(_ sender: UIButton) {
+        print("onCheckOrderButtonClicked")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 //        self.navigationController?.navigationBar.shadowImage = nil
+        backgroundView.applyBackgroundGradient()
         setCheckOrderViewUI()
     }
     
-    func setCheckOrderViewUI() {
+    private func setCheckOrderViewUI() {
         setTitleLabelUI("여름오렌지", UIColor(named: Constants.FruitfruitColors.orange1)!)
         setSecondaryLabelUI()
         setCheckOrderButtonUI()
     }
     
-    func setTitleLabelUI(_ tintedText: String, _ tintColor: UIColor) {
+    private func setTitleLabelUI(_ tintedText: String, _ tintColor: UIColor) {
         checkOrderTitleLabel.text = "여름오렌지 3개\n2,400원\n주문하시겠어요?"
         checkOrderTitleLabel.font = UIFont.preferredFont(for: .title1, weight: .bold)
     }
     
-    func setSecondaryLabelUI() {
+    private func setSecondaryLabelUI() {
         checkOrderSecondaryLabel.text = setSecondaryText()
         checkOrderSecondaryLabel.font = UIFont.preferredFont(for: .subheadline, weight: .bold)
         checkOrderSecondaryLabel.textColor = UIColor(named: Constants.FruitfruitColors.gray1)
         //TODO: func setCheckOrderTitleText(from text: String, colorText: String, color: UIColor) 추가 - 노아 merge 후
     }
     
-    func setSecondaryText() -> String {
+    private func setSecondaryText() -> String {
         let secondaryText = "주문 완료 후 입금이 확인되면\n내일 " + setSecondaryTextTime(13) + setSecondaryTextPlace("C5") + " 앞에 배달됩니다"
         return secondaryText
     }
     
-    func setSecondaryTextTime(_ timeInTwentyFourHour: Int) -> String {
+    private func setSecondaryTextTime(_ timeInTwentyFourHour: Int) -> String {
         var timeInTwelveHour = timeInTwentyFourHour
         if timeInTwelveHour > 12 {
             timeInTwelveHour = timeInTwelveHour - 12
@@ -53,15 +59,11 @@ class CheckOrderViewController: UIViewController {
         }
     }
     
-    func setSecondaryTextPlace(_ place: String) -> String {
+    private func setSecondaryTextPlace(_ place: String) -> String {
         return place
     }
     
-    @IBAction func onCheckOrderButtonClicked(_ sender: UIButton) {
-        print("onCheckOrderButtonClicked")
-    }
-    
-    func setCheckOrderButtonUI() {
+    private func setCheckOrderButtonUI() {
         let gradient = checkOrderButton.applyButtonGradient(colors: Constants.FruitfruitColors.buttonGradient)
         checkOrderButton.layer.insertSublayer(gradient, at: 0)
         checkOrderButton.setTitle("주문하기", for: .normal)
@@ -72,5 +74,9 @@ class CheckOrderViewController: UIViewController {
     }
     
     //TODO: Button 터치시 navigation
+    
+}
+
+extension CheckOrderViewController {
     
 }
