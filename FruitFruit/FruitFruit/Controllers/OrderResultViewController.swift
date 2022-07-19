8b532c1f77358ac57e5f4ba583a2b9c2429d8f97
Guel-git <eccyuna@gmail.com>
2022-07-19 23:22:47 +0900

    [feat] #19 titleLabel, secondaryTitleLabel 생성

--- FruitFruit/FruitFruit/Controllers/OrderResultViewController.swift
+++ FruitFruit/FruitFruit/Controllers/OrderResultViewController.swift
@@ -9,10 +9,26 @@ import UIKit
 
 class OrderResultViewController: UIViewController {
 
+    @IBOutlet weak var navigationTitleLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var secondaryTitleLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        navigationTitleLabel.text = "주문 완료"
+        navigationTitleLabel.textColor = UIColor(named: Constants.FruitfruitColors.black)
+        navigationTitleLabel.font = UIFont.preferredFont(for: .headline, weight: .semibold)
+        
+        titleLabel.text = "주문이\n완료되었습니다"
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = UIColor(named: Constants.FruitfruitColors.black)
+        titleLabel.font = UIFont.preferredFont(for: .title1, weight: .bold)
+        
+        secondaryTitleLabel.text = "아래 계좌로 입금해주세요"
+        secondaryTitleLabel.textColor = UIColor(named: Constants.FruitfruitColors.gray1)
+        secondaryTitleLabel.font = UIFont.preferredFont(for: .headline, weight: .bold)
+        
     }
     
 
