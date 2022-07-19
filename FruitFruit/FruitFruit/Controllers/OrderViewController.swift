17ba9a8030f7e2a730dce2121bae8c11bc291708
Willowwryu <tuyuyou@gmail.com>
2022-07-19 17:54:16 +0900

    뷰구성
    스태퍼 구현,

---
+++ FruitFruit/FruitFruit/Controllers/OrderViewController.swift
@@ -0,0 +1,32 @@
+//
+//  OrderViewController.swift
+//  FruitFruit
+//
+//  Created by 유재훈 on 2022/07/19.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    @IBOutlet var lblNumber: UILabel!
+    var Number = 1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        lblNumber.text = String(Number) + "개"
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func btnPlus(_ sender: UIButton) {
+        Number += 1 
+        lblNumber.text = String(Number) + "개"
+    }
+    @IBAction func btnMinus(_ sender: UIButton) {
+        if Number - 1 <= 0 {
+            return
+    }
+        Number -= 1
+        lblNumber.text = String(Number) + "개"
+    }
+
+}
