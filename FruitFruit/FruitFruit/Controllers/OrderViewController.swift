6092e0dbe9da9edb70f4756c9147365cd2dd1327
Willowwryu <tuyuyou@gmail.com>
2022-07-20 16:27:23 +0900

    구매하기버튼 구현
    오토레이아웃 미적용

--- FruitFruit/FruitFruit/Controllers/OrderViewController.swift
+++ FruitFruit/FruitFruit/Controllers/OrderViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class OrderViewController: UIViewController {
     @IBOutlet var lblNumber: UILabel!
     var Number = 1
-    
+    @IBOutlet var checkOrderButton: UIButton!
+    @IBAction func chekOrderbuttonOntap(_ sender: UIButton) {
+        print("onCheckOrderButtonClicked")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         view.applyBackgroundGradient()
         lblNumber.text = String(Number) + "개"
+        setCheckOrderButtonUI()
+
 
         // Do any additional setup after loading the view.
     }
@@ -29,8 +34,19 @@ class OrderViewController: UIViewController {
         Number -= 1
         lblNumber.text = String(Number) + "개"
     }
+    //checkOrderButton.titleLabel.text = "2400원 3개 구매"
 
     @IBAction func gotToFruitInfoView(_ sender: UIButton) {
         
     }
+    private func setCheckOrderButtonUI() {
+        let gradient = checkOrderButton.applyButtonGradient(colors: Constants.FruitfruitColors.buttonGradient)
+        checkOrderButton.layer.insertSublayer(gradient, at: 0)
+        checkOrderButton.setTitle("주문하기", for: .normal)
+        checkOrderButton.titleLabel?.font = UIFont.preferredFont(for: .headline, weight: .semibold)
+        checkOrderButton.layer.cornerRadius = 16
+        checkOrderButton.layer.borderWidth = 1
+        checkOrderButton.layer.borderColor = UIColor(named: Constants.FruitfruitColors.button1)?.cgColor
+    }
+
 }
