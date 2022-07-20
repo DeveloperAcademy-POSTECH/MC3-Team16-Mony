8bac3518cd97c75f7a99d86d99b822d8c59c3206
Guel-git <eccyuna@gmail.com>
2022-07-21 01:08:12 +0900

    [feat] #28 UI 그리기 완료

--- FruitFruit/FruitFruit/Controllers/ConfirmStatusViewController.swift
+++ FruitFruit/FruitFruit/Controllers/ConfirmStatusViewController.swift
@@ -10,19 +10,42 @@ import UIKit
 class ConfirmStatusViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var secondaryTitleLabel: UILabel!
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    let orderSheet: OrderSheet = {
+        let orderSheet = OrderSheet(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 48, height: 370))
+        orderSheet.translatesAutoresizingMaskIntoConstraints = false
+        return orderSheet
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
-        // Do any additional setup after loading the view.
+        navigationBar.shadowImage = UIImage()
     }
     
     private func setUI() {
         setBackground()
+        setLabels()
+        view.addSubview(orderSheet)
+        orderSheet.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24).isActive = true
+        orderSheet.topAnchor.constraint(equalTo: view.topAnchor, constant: 310).isActive = true
     }
     
     private func setBackground() {
         backgroundView.applyBackgroundGradient()
+    }
+    
+    private func setLabels() {
+        titleLabel.text = "입금을\n확인 중이에요!"
+        titleLabel.font = UIFont.preferredFont(for: .title1, weight: .bold)
+        titleLabel.textColor = UIColor(named: Constants.FruitfruitColors.black)
+        
+        secondaryTitleLabel.text = "오늘 오후 6시까지 입금이 확인되지 않으면\n주문이 자동으로 취소됩니다."
+        secondaryTitleLabel.font = UIFont.preferredFont(for: .subheadline, weight: .bold)
+        secondaryTitleLabel.textColor = UIColor(named: Constants.FruitfruitColors.gray1)
     }
 
     /*
