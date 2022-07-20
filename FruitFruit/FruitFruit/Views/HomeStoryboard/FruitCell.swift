187e8547cd593cdcf77ed9aaf1da2ce3f9e61cf6
PJunyeong <j_aion@naver.com>
2022-07-20 15:09:55 +0900

    #11 주문 카드 셀 코드화

--- FruitFruit/FruitFruit/Views/HomeStoryboard/FruitCell.swift
+++ FruitFruit/FruitFruit/Views/HomeStoryboard/FruitCell.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class FruitCell: UITableViewCell {
     static let identifier = "FruitCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented.")
+    }
+    
+    private func setUI(frame: CGRect) {
+        self.backgroundColor = .clear
+        self.layer.cornerRadius = 20
+        let fruitCellButtonView = FruitCellButtonView(frame: CGRect(x:0, y:0, width: frame.width, height: frame.height))
+        fruitCellButtonView.isUserInteractionEnabled = false
+        insertSubview(fruitCellButtonView, at: 0)
+        //TODO: 커스텀 Button -> 클릭 이벤트 모션 (Glowing) 실패 -> 유사 효과 구현하기
+    }
+    
 }
 
 //TODO: 테이블 뷰 사용 셀 커스텀하기
