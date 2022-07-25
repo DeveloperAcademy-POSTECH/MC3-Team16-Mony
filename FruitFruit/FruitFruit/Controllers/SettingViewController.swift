5193963bdabef2ff01ec7d2fefed138877457e1c
PJunyeong <JunyeongPark@MDOM9112-2.postech.ac.kr>
2022-07-25 21:22:04 +0900

    #37 feat : 프로필 이미지 뷰

--- FruitFruit/FruitFruit/Controllers/SettingViewController.swift
+++ FruitFruit/FruitFruit/Controllers/SettingViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class SettingViewController: UIViewController, UIGestureRecognizerDelegate {
     var isProfileEditing: Bool = false
+    
+    let fruitProfile: UIImageView = {
+        let profile = UIImageView()
+        profile.image = UIImage(named: Constants.FruitfruitImages.profile)
+        profile.translatesAutoresizingMaskIntoConstraints = false
+        return profile
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initSettingViewNavBar()
+        initProfile()
     }
     
     private func initSettingViewNavBar() {
@@ -21,7 +29,7 @@ class SettingViewController: UIViewController, UIGestureRecognizerDelegate {
         let backButtonImage = UIImage(systemName: "chevron.left")?.withTintColor(orangeColor, renderingMode: .alwaysOriginal)
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: backButtonImage, style: .done, target: self, action: #selector(popToPrevious))
         navigationItem.title = "프로필"
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: blackColor, NSAttributedString.Key.font: UIFont.preferredFont(for: .headline, weight: .semibold)]
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: blackColor, NSAttributedString.Key.font: UIFont.preferredFont(for: .headline, weight: .bold)]
         navigationController?.interactivePopGestureRecognizer?.delegate = self
         navigationController?.interactivePopGestureRecognizer?.isEnabled = true
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "수정하기", style: .done, target: self, action: #selector(editToggle))
@@ -43,6 +51,14 @@ class SettingViewController: UIViewController, UIGestureRecognizerDelegate {
             navigationItem.rightBarButtonItem?.title = "수정하기"
             navigationItem.rightBarButtonItem?.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: orangeColor], for: .normal)
         }
+    }
+    
+    private func initProfile() {
+        view.addSubview(fruitProfile)
+        fruitProfile.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 134).isActive = true
+        fruitProfile.topAnchor.constraint(equalTo: view.topAnchor, constant: 125).isActive = true
+        fruitProfile.widthAnchor.constraint(equalToConstant: 118).isActive = true
+        fruitProfile.heightAnchor.constraint(equalToConstant: 118).isActive = true
     }
 
     /*
