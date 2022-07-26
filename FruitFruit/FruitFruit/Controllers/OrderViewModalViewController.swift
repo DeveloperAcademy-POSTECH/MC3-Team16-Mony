6710a8db1d5acb9c9a32e382821438a486aef3a1
Willowwryu <tuyuyou@gmail.com>
2022-07-26 09:24:41 +0900

    OrderView모달뷰 생성
    생성

---
+++ FruitFruit/FruitFruit/Controllers/OrderViewModalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  OrderViewModalViewController.swift
+//  FruitFruit
+//
+//  Created by 유재훈 on 2022/07/26.
+//
+
+import UIKit
+
+class OrderViewModalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
