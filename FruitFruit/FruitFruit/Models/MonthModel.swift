dd77c0922b320c8b0da12ae3216d862e20b60c50
PJunyeong <JunyeongPark@Junyeongui-MacBookPro.local>
2022-07-26 19:22:16 +0900

    #46 feat : 공백 일 포함 체크

--- FruitFruit/FruitFruit/Models/MonthModel.swift
+++ FruitFruit/FruitFruit/Models/MonthModel.swift
@@ -29,14 +29,24 @@ struct MonthModel {
     }
     
     func checkWeekDay() {
+        //TODO: 해당 달의 특정 '날' -> 몇 주 차, 몇 요일인지 체크하는 함수
         let weekdayDict = ["일":0, "월":1, "화":2, "수":3, "목":4, "금":5, "토":6]
         let weekDayDictRev = [0:"일", 1:"월", 2:"화", 3:"수", 4:"목", 5:"금", 6:"토"]
         var curDayString = firstWeekDay
         var curDayOrder = weekdayDict[firstWeekDay]!
         var curDayInt = 1
+        
+        for curDay in 0..<curDayOrder {
+            print(0, curDay, "공백", weekDayDictRev[curDay]!)
+        }
+        
         for weekIdx in 0..<numOfWeeks {
-            for dayCnt in 0..<7 {
-                print(weekIdx, curDayOrder, curDayInt, curDayString)
+            for _ in 0..<7 {
+                if curDayInt > numOfDays {
+                    print(weekIdx, "공백", curDayInt, curDayString)
+                } else {
+                    print(weekIdx, curDayOrder, curDayInt, curDayString)
+                }
                 curDayInt += 1
                 if curDayOrder + 1 == 7 {
                     curDayOrder = 0
@@ -45,9 +55,6 @@ struct MonthModel {
                 }
                 curDayOrder += 1
                 curDayString = weekDayDictRev[curDayOrder]!
-                if curDayInt > numOfDays {
-                    return
-                }
             }
         }
     }
