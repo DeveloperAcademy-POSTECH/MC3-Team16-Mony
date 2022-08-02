9800de4b6b72bbbe2526d6541b11cd611cc535ec
PJunyeong <JunyeongPark@10-141-50-53.0200.pos.kr.iosda.org>
2022-08-02 09:09:08 +0900

    chore : 주석 제거

--- FruitFruit/FruitFruit/Controllers/HomeViewController.swift
+++ FruitFruit/FruitFruit/Controllers/HomeViewController.swift
@@ -275,9 +275,6 @@ class HomeViewController: UIViewController {
     private func setFruitOrderLayout(_ isTop: Bool) {
         view.constraints.filter{$0.identifier == "fruitOrderLabelTop"}.first?.constant = isTop ? 237 : 335
         self.view.layoutIfNeeded()
-//        UIView.animate(withDuration: 0.8, delay: 0.0, options: .curveEaseInOut, animations: {
-//            self.view.layoutIfNeeded()
-//        }, completion: nil)
     }
 }
 
